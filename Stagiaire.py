186d2c426eaa104f28518bedb76589fead8ade22
Ermich Reda <ermich.reda@ofppt-edu.ma>
2022-04-19 18:42:44 +0000

    first commit

---
+++ Stagiaire.py
@@ -0,0 +1,122 @@
+class Stagiaire:
+    nombre_stagiaires = 0
+    liste_stagiaires = []
+    def __init__(self, nom:str=None, prenom:str=None, filiere:str=None, note1:float=None, note2:float=None, note3:float=None):
+        self.__matricule = Stagiaire.nombre_stagiaires
+        self.__nom = nom
+        self.__prenom = prenom
+        self.__filiere = filiere
+        self.__note1 = note1
+        self.__note2 = note2
+        self.__note3 = note3
+        Stagiaire.nombre_stagiaires += 1
+        Stagiaire.liste_stagiaires.append(self)
+
+    def __del__(self):
+        Stagiaire.nombre_stagiaires -= 1
+
+    def __str__(self):
+        return "Matricle: {}, Nom: {}, Prenom: {}, Filiere: {}, Note1: {}, Note2: {}, Note3: {}".format(self.__matricule, self.__nom, self.__prenom, self.__filiere, self.__note1, self.__note2, self.__note3)
+
+    def __eq__(self, other):
+        return self.matricule == other.matricule
+
+    @property
+    def matricule(self):
+        return self.__matricule
+
+    @matricule.setter
+    def matricule(self, matricule):
+        self.__matricule=matricule
+
+    @property
+    def nom(self):
+        return self.__nom
+
+    @nom.setter
+    def nom(self, nom):
+        self.__nom = nom
+
+    @property
+    def prenom(self):
+        return self.__prenom
+
+    @prenom.setter
+    def prenom(self, prenom):
+        self.__prenom = prenom
+
+    @property
+    def filiere(self):
+        return self.__filiere
+
+    @filiere.setter
+    def filiere(self, filiere):
+        self.__filiere = filiere
+
+    @property
+    def note1(self):
+        return self.__note1
+
+    @note1.setter
+    def note1(self, note1):
+        self.__note1 = note1
+
+    @property
+    def note2(self):
+        return self.__note2
+
+    @note2.setter
+    def note2(self, note2):
+        self.__note2 = note2
+
+    @property
+    def note3(self):
+        return self.__note3
+
+    @note3.setter
+    def note3(self, note3):
+        self.__note3 = note3
+
+    # methode qui calcul la moyenne d'un stagiaire
+    def calculMoyenne(self):
+        return (self.__note1 + self.__note2 + self.__note3) / 3
+
+    # methode qui rechercher un stagiaire par son nom
+    @classmethod
+    def rechercher_stagiaire(cls, nom:str):
+        for stagiaire in cls.liste_stagiaires:
+            if stagiaire.nom == nom:
+                return stagiaire
+
+    # methode qui supprimer un stagiaire
+    @classmethod
+    def supprimer_stagiaire(cls, nom):
+        if cls.rechercher_stagiaire(nom) is not None:
+            cls.liste_stagiaires.remove(cls.rechercher_stagiaire(nom))
+        else:
+            print(f"Stagiaire '{nom}' n'existe pas")
+
+    # methode qui afficher la meilleure moyenne
+    @classmethod
+    def meilleure_moyenne(cls):
+        moyenne = 0
+        meilleur_stagiaire = None
+        for stagiaire in cls.liste_stagiaires:
+            if stagiaire.calculMoyenne() > moyenne:
+                moyenne = stagiaire.calculMoyenne()
+                meilleur_stagiaire = stagiaire
+        return meilleur_stagiaire
+
+    # methode qui afficher les stagiaires
+    @classmethod
+    def afficher_stagiaires(cls):
+        if len(cls.liste_stagiaires) == 0:
+            print("Aucun stagiaire")
+        else:
+            for stagiaire in cls.liste_stagiaires:
+                print(stagiaire)
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,63 @@
+from gestionStagiaire.Stagiaire import Stagiaire
+
+
+while True:
+    print("""
+    1-Afficher tous les stagiaires
+    2-Ajouter un stagiaire
+    3-Rechercher un stagiaire
+    4-Modifier un stagiaire
+    5-Supprimer un stagiaire
+    6-Afficher la meilleure moyenne parmi tous les stagiaires
+    7-Quitter
+    """)
+
+    choix=input("Votre choix: ")
+
+    if choix=="1":
+        Stagiaire.afficher_stagiaires()
+
+    elif choix=="2":
+        nouveau_stagiaire=Stagiaire()
+        nouveau_stagiaire.nom = input("Nouveau Nom: ")
+        nouveau_stagiaire.prenom = input("Nouveau Prenom: ")
+        nouveau_stagiaire.note1 = float(input("Nouveau Note1: "))
+        nouveau_stagiaire.note2 = float(input("Nouveau Note2: "))
+        nouveau_stagiaire.note3 = float(input("Nouveau Note3: "))
+
+    elif choix=="3":
+        while True:
+            nom_rechercher=input("Le nom du stagiaire vous devez rechercher. pour quitter[x] : ")
+            stagiaire_rechercher=Stagiaire.rechercher_stagiaire(nom_rechercher)
+            if stagiaire_rechercher is not None:
+                print(stagiaire_rechercher)
+            else:
+                print(f"\nStagiaire '{nom_rechercher}' n'existe pas\n")
+
+            if nom_rechercher.lower()=="x":
+                break
+
+    elif choix=="4":
+        nom_encienne=input("Le nom du stagiaire vous devez modifier : ")
+        stagiaire_modifie= Stagiaire.rechercher_stagiaire(nom_encienne)
+        if stagiaire_modifie is None:
+            print("\nLe stagiaire n'existe pas\n")
+        else:
+            stagiaire_modifie.nom=input("Nouveau Nom: ")
+            stagiaire_modifie.prenom=input("Nouveau Prenom: ")
+            stagiaire_modifie.note1=float(input("Nouveau Note1: "))
+            stagiaire_modifie.note2=float(input("Nouveau Note2: "))
+            stagiaire_modifie.note3=float(input("Nouveau Note3: "))
+
+    elif choix=="5":
+        while True:
+            nom_supprimer = input("Le nom du stagiaire vous devez supprimer. pour quitter[x]: ")
+            Stagiaire.supprimer_stagiaire(nom_supprimer)
+            if nom_supprimer.lower()=="x":
+                break
+
+    elif choix=="6":
+        print(Stagiaire.meilleure_moyenne())
+
+    elif choix=="7":
+        break
